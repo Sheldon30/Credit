f1cc9760ae7ca1413c9a4b5c1df12ec14059c915
Sheldon <aneln700@gmail.com>
2023-03-20 20:56:01 +0300

    Credit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,2 +1,10 @@
 public class CreditPaymentService {
+    public double calculate(double summa, double p, double n) {
+        //n - количество месяцев
+        //r - процент
+        double r = p / 100 / 12;
+        double pay = summa * ((r * Math.pow((1 + r), n)) / (Math.pow((1 + r), n) - 1));
+        return (int) pay;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CreditPaymentService service = new CreditPaymentService();
+        double pay = service.calculate(1000000,9.99,12 );
+        System.out.println("Ежемесячный платеж: " + pay);
+        System.out.println();
+        System.out.println("Ежемесячный платеж: " + service.calculate(1000_000, 9.99, 24));
+        System.out.println();
+        System.out.println("Ежемесячный платеж: " + service.calculate(1000_000, 9.99, 36));
     }
 }
